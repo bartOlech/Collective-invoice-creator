c389c542b2fc30621db3e4694d16edc83dd66a04
Bartłomiej Olech <olech.bartlomiej@gmail.com>
2021-08-05 14:34:48 +0200

    create env variables

--- apka/views.py
+++ apka/views.py
@@ -24,16 +24,15 @@ def getOrders(request):
     date = request.POST['date'];
     timestamp = str(time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple()))
 
+    print(BASELINKER_API)
+
     parameters = '{"date_from": "'+timestamp+'"}'
 
-    # NIE DZIAŁA PRZEKAZANIE TOKENA Z .ENV (CHYBA DLATEGO, ŻE JEST Z "", GDY PRRZEKAZUJE ZMIENNĄ DO JSON)
     data = {
-        'token': '2003256-2007225-9HN7QVLWH8WRE6H736FOKJ3U6XZ78DL3LN7MCQE8IQYXH1VEX6Z0OBYCTLN1OM7J',
+        'token': BASELINKER_API,
         'method': 'getOrders',
         'parameters': parameters
     }
-
-    # POBIERA ZAMÓWIENIA Z DATY JAKĄ WYBIORĘ, TERAZ TRZEBA GENERWOAĆ Z TEGO EXCEL, POTEM TRZEBA ŻEBY TYLKO Z DANEGO STATUSU (MOŻE)
 
     response = requests.post('https://api.baselinker.com/connector.php', data=data)
 
